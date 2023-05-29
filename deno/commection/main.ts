4b8c9de3ab3ce4a2d3cb96c0eb6874508f7897e6
narumincho <16481886+narumincho@users.noreply.github.com>
2023-05-29 06:39:17 +0000

    add schema type in deno

--- deno/commection/main.ts
+++ deno/commection/main.ts
@@ -5,6 +5,8 @@ export type Server<ImplementType, RequestExpr> = {
 
 export type Schema<ImplementType, RequestExpr> = {
   readonly name: string;
+  readonly typeDefinitions: ReadonlyArray<TypeDefinition>;
+  readonly functionDefinitions: ReadonlyArray<FunctionDefinition>;
   readonly implementType: PhantomData<ImplementType>;
   readonly requestExpr: PhantomData<RequestExpr>;
 };
@@ -12,4 +14,51 @@ export type Schema<ImplementType, RequestExpr> = {
 export type PhantomData<T> = {
   /** Do not create with this field */
   readonly __bland: T;
+};
+
+export type TypeDefinition = {
+  readonly id: string;
+  readonly name: string;
+  readonly description: string;
+  readonly attribute: TypeAttribute | undefined;
+  readonly body: TypeStructure;
+};
+
+export type TypeAttribute = "";
+
+export type TypeStructure = {
+  readonly type: "sum";
+  readonly pattern: ReadonlyArray<Pattern>;
+} | {
+  readonly type: "product";
+  readonly fields: ReadonlyArray<Field>;
+};
+
+export type Pattern = {
+  readonly type: Type;
+  readonly parameter: ReadonlyArray<string>;
+  readonly arguments: ReadonlyArray<Type>
+};
+
+export type Field = {
+  readonly name: string;
+  readonly description: string;
+  readonly type: Type;
+};
+
+export type FunctionDefinition = {
+  readonly name: string;
+  readonly parameters: ReadonlyArray<FunctionParameter>;
+  readonly resultType: Type;
+  readonly typeParameter: ReadonlyArray<string>;
+};
+
+export type FunctionParameter = {
+  readonly name: string;
+  readonly type: Type;
+};
+
+export type Type = {
+  readonly id: string;
+  readonly arguments: ReadonlyArray<Type>;
 };
